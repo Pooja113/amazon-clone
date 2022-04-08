3ce10a479e06a077e6525627cd3c817e2d8f811d
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2022-04-08 22:16:20 +0530

    Router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
+import { BrowserRouter as Router,Switch,Route } from 'react-router-dom';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
 
+
 function App() {
   return (
     //BEM
-    <div className="app">
-      <Header/>
-      <Home />
-    { /* Header */}
-
-    { /* Home */}
-    { /* Footer */}
-
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path='/checkout'>
+              <Header/>
+              <h1>Check Out</h1>
+          </Route>
+          <Route path='/'>
+            <Header/>
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
