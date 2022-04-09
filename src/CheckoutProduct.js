9a65bf6b2a382a76efb141668420e3e32e7d9d96
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2022-04-09 23:31:13 +0530

    saved

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import './CheckoutProduct.css'
 
-const CheckoutProduct = () => {
+const CheckoutProduct =  ({id, title,image,price,rating}) => {
   return (
-    <div>
-      
+    <div className='checkoutProduct'>
+      <img className='checkoutProduct__image' src={image} alt=''/>
+        <div className='checkoutProduct__info'>
+        <p className='checkoutProduct__title'>{title}</p>
+        <p className='checkoutProduct__price'>
+        <small>$</small>
+        <strong>{price}</strong>
+        </p>
+        <p className='checkoutProduct__rating'>{rating}</p>
+
+      </div>
     </div>
   )
 }
