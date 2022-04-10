0334993b9dfdaf4f9fecaaebf6e1a5185ad8bfa2
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2022-04-10 15:33:22 +0530

    Athentication

--- src/Header.js
+++ src/Header.js
@@ -4,11 +4,16 @@ import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 import { Link } from 'react-router-dom';
 import { useStateValue } from './StateProvider';
+import { auth } from './firebase';
 
 const Header = () => {
 
-  const [{basket},dispatch] = useStateValue();
-
+  const [{basket,user},dispatch] = useStateValue();
+  const handleAuthentication = () =>{
+    if(user){
+      auth.signOut();
+    }
+  }
   return (
     <div className='header'>
       <Link to="/">
@@ -22,10 +27,12 @@ const Header = () => {
           <SearchIcon className='header__searchIcon'/>
         </div>
         <div className='header__nav'>
-          <Link to="/login">
-            <div className='header__option'>
-              <span className='header__optionLineOne'>Hello Guest!</span>
-              <span className='header__optionLineTwo'>Sign In</span>
+          <Link to={!user && "/login"}>
+            <div className='header__option' onClick={handleAuthentication}>
+              <span className='header__optionLineOne'>Hello </span>
+              <span className='header__optionLineTwo'>
+              {user ? 'Sign Out': 'Sign In'}
+              </span>
             </div>
           </Link>
           <div className='header__option'>
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 const firebaseConfig = {
   apiKey: "AIzaSyCawYFo04BFeMRlMT0qy9vPqPFRE_FXBOM",
   authDomain: "clone-df53f.firebaseapp.com",
@@ -6,3 +10,10 @@ const firebaseConfig = {
   messagingSenderId: "445736918117",
   appId: "1:445736918117:web:e481660efc5c396034ba8c"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export {db , auth };
