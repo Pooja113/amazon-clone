c70d114f3c2a345fa705132baec7e99d7b389754
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2022-04-14 14:00:25 +0530

    changes

--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,6 @@ import { useStateValue } from './StateProvider';
 import { auth } from './firebase';
 
 const Header = () => {
-
   const [{basket,user},dispatch] = useStateValue();
   const handleAuthentication = () =>{
     if(user){
