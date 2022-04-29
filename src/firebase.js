66002363b6edda1962cd48140a57fd15c404b848
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2022-04-29 18:37:48 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,7 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCawYFo04BFeMRlMT0qy9vPqPFRE_FXBOM",
-  authDomain: "clone-df53f.firebaseapp.com",
-  projectId: "clone-df53f",
-  storageBucket: "clone-df53f.appspot.com",
-  messagingSenderId: "445736918117",
-  appId: "1:445736918117:web:e481660efc5c396034ba8c"
+ 
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
